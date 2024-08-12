71279043a22d30f8ceb6ee7d3a2458f0f9834ba2
Madhumitha Balaji <madhumithabalaji1@gmail.com>
2024-08-12 15:20:35 +0530

    first commit

---
+++ SCRIPT.js
@@ -0,0 +1,39 @@
+ /* //print statement
+ console.log("hello world");
+ //statements
+ let x,y,z;
+ x=10;
+ y=20;
+ z=x+y;
+ console.log(z);
+ //white spaces
+ let a_rand_str = " hello world";
+ console.log(a_rand_str);
+ //variables
+let l=10;
+l=l*l;
+console.log(l);
+// comment statement == this will not be executed
+// case sensitivity
+let a_rand_str2 = "aaron";
+let A_Rand_Str2 = "paxton";
+console.log(a_rand_str2); 
+console.log(A_Rand_Str2);*/
+/*  function fu()
+ {
+    var x=10 ;
+    let y=10;
+    console.log(x);
+    console.log(y);
+
+ }
+ fu();
+ //console.log(x);
+ //o/p: ReferenceError: x is not defined
+ //console.log(y); */
+ let n=10;
+ n+=100;
+ console.log(n);
+ //
+ 
+
